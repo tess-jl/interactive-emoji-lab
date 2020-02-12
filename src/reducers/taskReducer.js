15291b23d6244b70b1af23fda3c4a95696ea17f9
Tess Lameyer <tjlameyer@gmail.com>
2020-02-12 14:11:00 -0800

    reducer fully tested

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -13,8 +13,8 @@ export default function reducer(state, action) {
       return { ...state, snacks: state.snacks + 1 };
     case TAKE_NAP:
       return { ...state, naps: state.naps + 1 };
-
-    
+    case STUDY: 
+      return { ...state, studies: state.studies + 1 };
     default: 
       return state; 
   }
