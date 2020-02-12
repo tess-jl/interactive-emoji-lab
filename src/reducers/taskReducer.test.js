d73f84595914ae2083f2ccb85d1faf296f24fcf0
Tess Lameyer <tjlameyer@gmail.com>
2020-02-12 14:06:11 -0800

    ready to TDD the drink snack case of the reducer

--- src/reducers/taskReducer.test.js
+++ src/reducers/taskReducer.test.js
@@ -10,6 +10,14 @@ describe('mood reducer tests', () => {
 
     expect(newState).toEqual({ coffees: 2 });
   });
+  it('handles the EAT_SNACK action', () => {
+    const action = eatSnack();
+    const initialState = { snacks: 1 };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ snacks: 2 });
+  });
 
 
 
