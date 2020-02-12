007ebbfa7877300a5fc31a3a392adad33f64f65d
Tess Lameyer <tjlameyer@gmail.com>
2020-02-12 14:07:26 -0800

    handles eat snack correctly

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -9,9 +9,12 @@ export default function reducer(state, action) {
   switch(action.type) {
     case DRINK_COFFEE:
       return { ...state, coffees: state.coffees + 1 };
+    case EAT_SNACK:
+      return { ...state, snacks: state.snacks + 1 };
 
     
     default: 
       return state; 
   }
 }
+
