8a74f3037847085ae4ea271659b7536ebf9588c6
Tess Lameyer <tjlameyer@gmail.com>
2020-02-12 14:01:19 -0800

    ready to write the drink coffee case

--- src/reducers/taskReducer.test.js
+++ src/reducers/taskReducer.test.js
@@ -2,6 +2,19 @@ import reducer from './taskReducer';
 import { drinkCoffee, eatSnack, takeNap, study } from '../actions/taskActions';
 
 describe('mood reducer tests', () => {
+  it('handles the DRINK_COFFEE action', () => {
+    const action = drinkCoffee();
+    const initialState = { coffees: 1 };
+
+    const newState = reducer(initialState, action);
+
+    expect(newState).toEqual({ coffees: 2 });
+  });
+
+
+
+
+
   it('handles the action it does not understand gracefully', () => {
     const action = { type: 'WRITE_CODE' };
     const initialState = { coffees: 1 };
@@ -10,7 +23,5 @@ describe('mood reducer tests', () => {
 
     expect(newState).toEqual({ coffees: 1 });
   });
-
-
 
 });
