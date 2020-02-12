829adb7bdce51e0732fb753c6fef6cbe685764c7
Tess Lameyer <tjlameyer@gmail.com>
2020-02-12 14:05:09 -0800

    drinks coffee sucessfully

--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -7,6 +7,10 @@ import {
 
 export default function reducer(state, action) {
   switch(action.type) {
+    case DRINK_COFFEE:
+      return { ...state, coffees: state.coffees + 1 };
+
+    
     default: 
       return state; 
   }
